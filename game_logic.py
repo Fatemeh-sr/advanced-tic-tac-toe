0a6ba1f3abd565984d170a0e1daba835f5e70f38
Fatemeh-sr <Fatemeh.srvni8@gmail.com>
2026-02-04 01:47:33 +0330

    user-moving

--- game_logic.py
+++ game_logic.py
@@ -1,26 +1,40 @@
+# move = {"a1": 0, "b1": 0, "a2": 0, "b2": 1, "symbol_first": "X"}
+
+
+class Move:
+    def __init__(self, a1, b1, a2, b2, symbol_first):
+        self.a1 = a1
+        self.b1 = b1
+        self.a2 = a2
+        self.b2 = b2
+        self.symbol_first = symbol_first
+
+    # متد برای اعمال حرکت روی board
+    def apply(self, board):
+        symbol_second = "O" if self.symbol_first == "X" else "X"
+        board[self.a1][self.b1] = self.symbol_first
+        board[self.a2][self.b2] = symbol_second
+
+
 board = [
     ["-", "-", "-", "-"],
     ["-", "-", "-", "-"],
     ["-", "-", "-", "-"],
     ["-", "-", "-", "-"],
 ]
-move = {"a1": 0, "b1": 0, "a2": 0, "b2": 1, "symbol_first": "X"}
 
+# ایجاد حرکت‌ها
+move1 = Move(0, 0, 0, 1, "X")  # دومینو X-O
+move2 = Move(3, 0, 2, 0, "O")  # دومینو O-X
 
-def moving(board, move):
-    # انتخاب نماد دوم
-    symbol_second = "O" if move["symbol_first"] == "X" else "X"
-
-    # قرار دادن نمادها روی board
-    board[move["a1"]][move["b1"]] = move["symbol_first"]
-    board[move["a2"]][move["b2"]] = symbol_second
-
-
-moving(board, move)
-board2 = [row[:] for row in board]
-move = {"a1": 3, "b1": 0, "a2": 2, "b2": 1, "symbol_first": "O"}
-moving(board, move)
+# اعمال حرکت‌ها
+move1.apply(board)
+move2.apply(board)
+Move(3, 1, 3, 2, "X").apply(board)
 
 # چاپ board
 for row in board:
     print(" ".join(row))
+
+def get_user_move(board) :
+    #Hi
