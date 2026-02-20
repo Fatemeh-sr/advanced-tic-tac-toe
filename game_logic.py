38224322cbd402198b4a5b30b30d73b0660a71df
Fatemeh-sr <Fatemeh.srvni8@gmail.com>
2026-02-21 02:11:25 +0330

    delete files

--- game_logic.py
+++ game_logic.py
@@ -9,11 +9,38 @@ class Move:
         self.b2 = b2
         self.symbol_first = symbol_first
 
-    # متد برای اعمال حرکت روی board
     def apply(self, board):
         symbol_second = "O" if self.symbol_first == "X" else "X"
         board[self.a1][self.b1] = self.symbol_first
         board[self.a2][self.b2] = symbol_second
+
+
+# تابع اعتبارسنجی
+def is_valid_move(board, move):
+    r1, c1 = move.a1, move.b1
+    r2, c2 = move.a2, move.b2
+
+    if not (0 <= r1 < 4 and 0 <= c1 < 4 and 0 <= r2 < 4 and 0 <= c2 < 4):
+        return False
+    if board[r1][c1] != "-" or board[r2][c2] != "-":
+        return False
+    if abs(r1 - r2) + abs(c1 - c2) != 1:
+        return False
+    return True
+
+
+# تابع wrapper برای ورودی کاربر / تست
+def user_move(board, r1, c1, r2, c2, symbol_first):
+    try:
+        move = Move(r1, c1, r2, c2, symbol_first)
+        if not is_valid_move(board, move):
+            print("حرکت نامعتبر است!")
+            return False
+        move.apply(board)
+        return True
+    except Exception as e:
+        print("خطا در حرکت:", e)
+        return False
 
 
 board = [
@@ -35,32 +62,3 @@ Move(3, 1, 3, 2, "X").apply(board)
 # چاپ board
 for row in board:
     print(" ".join(row))
-
-
-def user_move(board, move):
-    first_cell = (a1, a1)
-    second_cell = (a2, b2)
-
-    (
-        move(a1, ba, a2, b2, fiest_symbol) if board[a1][b1] == "-" else print("erorr")
-    )  # need try - except
-
-
-def is_valid_move(board, move):
-
-    r1, c1 = move.a1, move.b1
-    r2, c2 = move.a2, move.b2
-
-    # 1. داخل صفحه بودن
-    if not (0 <= r1 < 4 and 0 <= c1 < 4 and 0 <= r2 < 4 and 0 <= c2 < 4):
-        return False
-
-    # 2. خالی بودن خانه‌ها
-    if board[r1][c1] != "-" or board[r2][c2] != "-":
-        return False
-
-    # 3. مجاور بودن (فقط افقی یا عمودی)
-    if abs(r1 - r2) + abs(c1 - c2) != 1:
-        return False
-
-    return True
